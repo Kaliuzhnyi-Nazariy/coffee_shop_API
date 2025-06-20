c64a9d42a8059e9762f25338ee359875f3048466
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-06-20 17:38:52 +0300

    update payment validations

--- src/models/validatons/paymentValidation.ts
+++ src/models/validatons/paymentValidation.ts
@@ -4,6 +4,7 @@ const CardSchema = z.object({
   type: z.literal("card"),
   cardNumber: z.string().min(12),
   cvc: z.string().length(3),
+  cardHolderName: z.string(),
   brand: z.string(),
   expiryMonth: z.number().min(1).max(12),
   expiryYear: z.string(),
@@ -11,7 +12,9 @@ const CardSchema = z.object({
 
 const PayPalSchema = z.object({
   type: z.literal("paypal"),
-  email: z.email(),
+  email: z.string().email(),
+  payerId: z.string().optional(),
+  transactionId: z.string().optional(),
 });
 
 const paymentValidation = z.discriminatedUnion("type", [
