ca3ab1e3ba7ac250697c6fa7997fed8a21ab6405
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-01 11:57:45 +0300

    change error message

--- src/models/validatons/user/createUser.validation.ts
+++ src/models/validatons/user/createUser.validation.ts
@@ -11,7 +11,7 @@ const createUserValidation = z.object({
     .max(16, "Password max length is 16 characters!")
     .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,16}$/, {
       message:
-        "Must contain 1 number, 1 special sign, 1 capital letter and be 6-16 characters",
+        "Password must contain 1 number, 1 special sign, 1 capital letter and be 6-16 characters",
     }),
   location: z.string().optional(),
 });
